b61bf6960acfdda714e9c4c4ccd083581230217f
Maanit5117 <mmittra5117@gmail.com>
2026-03-28 23:07:16 +0530

    Welcome screen completion

---
+++ app/src/main/java/com/example/newcoffeeapp/screens/welcomeScreen/WelcomeScreen.kt
@@ -0,0 +1,70 @@
+package com.example.newcoffeeapp.screens.welcomeScreen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.newcoffeeapp.R
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+
+
+fun WelcomeScreen() {
+
+    Box(
+        modifier = Modifier.fillMaxSize().background(color = Color.White)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.welcome_screen_photo),
+            contentDescription = "Welcome Image",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+
+        Column(
+            modifier = Modifier.fillMaxSize().padding(bottom = 40.dp),
+            horizontalAlignment = CenterHorizontally,
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            Text(text = "  Freshly Brewed!!", fontWeight = FontWeight.Bold, fontSize = 45.sp)
+
+            Spacer(modifier = Modifier.height(80.dp))
+
+
+            Button(
+                onClick = { },
+                modifier = Modifier.padding(30.dp).fillMaxWidth().height(50.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Black
+                ),
+                shape = RoundedCornerShape(10.dp)
+            ) {
+                Text(text = "Get Started", fontWeight = FontWeight.Bold, fontSize = 25.sp)
+            }
+        }
+     }
+}
