4a91290ccaa03a3701d8bef9a89a4145503ab569
Maanit5117 <mmittra5117@gmail.com>
2026-03-28 23:48:20 +0530

    added bottom nav bar

--- app/src/main/java/com/example/newcoffeeapp/MainActivity.kt
+++ app/src/main/java/com/example/newcoffeeapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.newcoffeeapp.screens.HomeScreen.HomeScreen
 import com.example.newcoffeeapp.ui.theme.NewCoffeeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +20,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NewCoffeeAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    HomeScreen()
                 }
             }
         }
     }
-}
+
 
